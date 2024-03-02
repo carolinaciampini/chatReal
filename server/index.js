cd25106eb6b486fafd191177beda54685a4f3d3a
Carolina Ciampini <carolinaciampini02@gmail.com>
2024-03-02 19:46:14 -0300

    chat

--- server/index.js
+++ server/index.js
@@ -1,9 +1,31 @@
 import express from "express"
 import logger from "morgan"
 
+import { Server } from "socket.io"
+import {createServer } from "node:http"
+
+
 
 const port = process.env.PORT ?? 3000
+
+
 const app = express()
+const server = createServer(app)
+const io = new Server(server) // io > IN OUT bidireccional 
+
+io.on("connection", (socket) => {
+  console.log("an user has been connected")
+
+  socket.on("disconnect", () => {
+    console.log("an user has disconnected")
+  })
+
+  socket.on("chat message", (msg) => {
+      io.emit("chat message", msg )
+  })
+})
+
+
 app.use(logger("dev"))
 
 
@@ -11,6 +33,6 @@ app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/client/index.html")
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log( `Server listening on port ${port} ` )
 })
